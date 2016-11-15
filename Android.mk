cee4dbf3cac3195cc503a15e2272fc4fb27d65ad
hejunweihehe <846255299@qq.com>
2016-11-15 10:09:26 +0800

    apk名字修改

--- Android.mk
+++ Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += src/com/mediatek/FMRadio/IFMRadioService.aidl
 LOCAL_SRC_FILES += src/com/android/factoryservice/IFmNative.aidl
 
-LOCAL_PACKAGE_NAME := LTEDiscovery
+LOCAL_PACKAGE_NAME := LTE
 LOCAL_CERTIFICATE := platform
 LOCAL_JNI_SHARED_LIBRARIES := libfmjni
 LOCAL_JNI_SHARED_LIBRARIES := libps-jni
